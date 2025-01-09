18ed7db02de5030b91812a4e550c9a1b86be62d2
itai234 <itaitaiy1@gmail.com>
2025-01-09 15:36:01 +0200

    check if commits from second pc and pushes

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("checks , if commit and push works ");
+Console.WriteLine("checks , if commit and push works and from the second pc ");
