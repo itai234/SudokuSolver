5dd167596583a00b28d70fb03ba1356a56efaa5d
itai234 <itaitaiy1@gmail.com>
2025-01-21 13:01:03 +0200

    uploading dev

---
+++ SudokuSolver/Exceptions/UnsolvableBoardException.cs
@@ -0,0 +1,16 @@
+﻿using SudokuSolver.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Exceptions;
+
+public class UnsolvableBoardException : SudokuExceptions
+{
+    public UnsolvableBoardException(string message) : base(message)
+    {
+
+    }
+}
