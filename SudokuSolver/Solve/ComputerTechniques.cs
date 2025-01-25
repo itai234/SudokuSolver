590948b75caa924effc6bc59cc53bd480d413fd2
itai234 <itaitaiy1@gmail.com>
2025-01-25 14:26:39 +0200

    finished the backtracking , it solves most of the puzzles , but for unsolvable puzzles it has a problem that need to be fixed.

--- SudokuSolver/Solve/ComputerTechniques.cs
+++ SudokuSolver/Solve/ComputerTechniques.cs
@@ -1,4 +1,5 @@
 ﻿using SudokuSolver.DataStructures.Board;
+using SudokuSolver.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -17,17 +18,18 @@ namespace SudokuSolver.Solve;
 public class ComputerTechniques<T> : ISolving<T>
 {
     private SudokuBoard<T> sudokuBoard;
-    HashSet<T> possibilities;
     HumanTechniques<T> humanTechniques;
+    private HashSet<string> visitedStates = new HashSet<string>();
+
 
     /// <summary>
     /// sets the sudoku board class instance to be in this class for easy access.
+    /// also resets the human techniques instance.
     /// </summary>
     /// <param name="sudokuBoard"></param>
     public void SetBoard(SudokuBoard<T> sudokuBoard)
     {
         this.sudokuBoard = sudokuBoard;
-        possibilities = sudokuBoard.CreatePossibilitySet();
         humanTechniques = new HumanTechniques<T>();
         humanTechniques.SetBoard(sudokuBoard);
     }
@@ -41,28 +43,34 @@ public class ComputerTechniques<T> : ISolving<T>
         didSolve = BackTracking();
         Validation.ValidateBoard<T>.Validate(
         sudokuBoard.board, Utilities.SudokuBoardUtilities.GameStateForValidation.BaseBoardWithPossibilitiesFixed);
+        if (didSolve == false) throw new UnsolvableBoardException("Board is Unsolvable.");
         return didSolve;
 
     }
-
+    /// <summary>
+    /// the function backtracks and uses brute forcing for the values, 
+    /// also using the human techniques to solve the board.
+    /// </summary>
+    /// <returns></returns>
     private bool BackTracking()
     {
-        Validation.ValidateBoard<T>.Validate(
-      sudokuBoard.board, Utilities.SudokuBoardUtilities.GameStateForValidation.BaseBoardWithPossibilitiesFixed);
-
+        
         if (sudokuBoard.IsBoardSolved())
             return true;
 
         var cell = sudokuBoard.FindCellWithLeastPossibilities();
         if (cell == null)
             return false;
-
+    
         int Row = cell.Value.row;
         int Col = cell.Value.col;
         HashSet<T> cellPossibilities = sudokuBoard.board[Row, Col].GetPossibilities();
 
-
-
+        string CurrentState  = GetBoardState();
+        if (visitedStates.Contains(CurrentState))
+            return false;
+        visitedStates.Add(CurrentState);
+       
         foreach (T possibility in cellPossibilities)
         {
             if (sudokuBoard.CanPlaceValue(Row, Col, possibility))
@@ -75,6 +83,7 @@ public class ComputerTechniques<T> : ISolving<T>
                 try
                 {
                     humanTechniques.Solve();
+                    if(sudokuBoard.IsBoardSolved()) return true;
                     if (BackTracking())
                         return true;
 
@@ -88,6 +97,24 @@ public class ComputerTechniques<T> : ISolving<T>
             }
         }
         return false;
+    }
+
+    /// <summary>
+    /// the function returns a string representing the board state. 
+    /// the purpose is to not go to loops in the backtracking
+    /// </summary>
+    /// <returns></returns>
+    private string GetBoardState()
+    {
+        StringBuilder boardString = new StringBuilder();
+        for (int row = 0; row < sudokuBoard.Size; row++)
+        {
+            for (int col = 0; col < sudokuBoard.Size; col++)
+            {
+                boardString.Append(sudokuBoard.board[row, col].IsPermanent()? sudokuBoard.board[row, col].GetValue().ToString(): "0"); 
+            }
+        }
+        return boardString.ToString();
     }
 }
 
